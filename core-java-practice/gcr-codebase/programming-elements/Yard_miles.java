9c0a7e195fab9294ad446d9f9ed6c503e469da5a
mHarshMittal <mittalharsh839@gmail.com>
2025-12-18 12:51:58 +0530

    Added problems

---
+++ core-java-practice/gcr-codebase/programming-elements/Yard_miles.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Yard_miles{
+ public static void main(String[] args){
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter distance in feet: ");
+ int dist = sc.nextInt();
+   double yard = dist / 3;
+   double mile = yard / 1760;
+   System.out.println("The distance in yards is "+yard+" while the distance in miles is "+mile);
+}
+}
