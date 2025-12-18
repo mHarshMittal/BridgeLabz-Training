3a28e431049d04c9375e95c865fd500a69aea59b
mHarshMittal <mittalharsh839@gmail.com>
2025-12-18 10:57:54 +0530

    Added problems

---
+++ core-java-practice/gcr-codebase/programming-elements/DistanceToMiles.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Distance{
+ public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   int dist = sc.nextInt();
+   double miles = dist * 0.621371;
+   System.out.print("The total miles is "+miles+" mile for the given "+dist+" km");
+}
+}
