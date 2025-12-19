dbcc81f94c9a31681639b55f6e05c15798d71c7c
mHarshMittal <mittalharsh839@gmail.com>
2025-12-19 11:15:43 +0530

    Refactored code

--- core-java-practice/gcr-codebase/programming-elements/TotalPerPrice.java
+++ core-java-practice/gcr-codebase/programming-elements/TotalPerPrice.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-public class TotalPerPrice{
- public static void main(String[] args){
- Scanner sc = new Scanner(System.in);
- System.out.println("Enter unit price: ");
- int unit = sc.nextInt();
- System.out.println("Enter quantiit: ");
- int quant = sc.nextInt();
-   int net = quant*unit;
-   System.out.println(" The total purchase price is INR "+net+" if the quantity "+quant+" and the unit price is INR "+unit);
-}
+
+public class TotalPerPrice {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter unit price: ");
+    int unit = sc.nextInt();
+    System.out.println("Enter quantiit: ");
+    int quant = sc.nextInt();
+    int net = quant * unit;
+    System.out.println(
+        " The total purchase price is INR " + net + " if the quantity " + quant + " and the unit price is INR " + unit);
+  }
 }
