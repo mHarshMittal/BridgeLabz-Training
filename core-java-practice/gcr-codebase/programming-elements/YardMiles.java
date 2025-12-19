f8f83ce56f394985869095a43ba7e7f959ba09fa
mHarshMittal <mittalharsh839@gmail.com>
2025-12-19 11:26:27 +0530

    Rename Files

--- core-java-practice/gcr-codebase/programming-elements/YardMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/YardMiles.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class YardMiles {
+public class YardMiles{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter distance in feet: ");
