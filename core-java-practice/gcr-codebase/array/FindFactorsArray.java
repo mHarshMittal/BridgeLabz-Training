13bedbc9406064b6862233df4311fc59e7d5c786
mHarshMittal <mittalharsh839@gmail.com>
2025-12-20 17:40:46 +0530

    [Harsh Mittal]Added .add Arrays Level 1

---
+++ core-java-practice/gcr-codebase/array/FindFactorsArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int[] factors = new int[n];
+        int index = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0){
+            factors[index]=i;
+            index++;
+            }
+        }
+        for (int i = 0; i < index; i++) {
+            System.out.print("Factors: "+ factors[i] + " ");
+        }
+    }
+}
