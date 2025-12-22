b785c445eaa0884d82f9e4cee0322c0715a108bd
mHarshMittal <mittalharsh839@gmail.com>
2025-12-22 15:16:24 +0530

    [Harsh Mittal] Added. add Substring Comparision level 1

--- core-java-practice/gcr-codebase/strings/SubstrCompare.java
+++ core-java-practice/gcr-codebase/strings/SubstrCompare.java
@@ -1,37 +1,37 @@
 import java.util.Scanner;
 
-public class SubstringComparison {
+public class SubstrCompare {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String text = scanner.next();
+        String text = sc.next();
         System.out.print("Enter start index: ");
-        int start = scanner.nextInt();
+        int start = sc.nextInt();
         System.out.print("Enter end index: ");
-        int end = scanner.nextInt();
+        int end = sc.nextInt();
 
-        String customSubstring = "";
+        String customSubstr = "";
         for (int i = start; i < end && i < text.length(); i++) {
-            customSubstring += text.charAt(i);
+            customSubstr += text.charAt(i);
         }
 
         String builtInSubstring = text.substring(start, Math.min(end, text.length()));
 
         boolean areEqual = true;
-        if (customSubstring.length() != builtInSubstring.length()) {
+        if (customSubstr.length() != builtInSubstring.length()) {
             areEqual = false;
         } else {
-            for (int i = 0; i < customSubstring.length(); i++) {
-                if (customSubstring.charAt(i) != builtInSubstring.charAt(i)) {
+            for (int i = 0; i < customSubstr.length(); i++) {
+                if (customSubstr.charAt(i) != builtInSubstring.charAt(i)) {
                     areEqual = false;
                     break;
                 }
             }
         }
 
-        System.out.println("Custom Substring: " + customSubstring);
+        System.out.println("Custom Substring: " + customSubstr);
         System.out.println("Built-in Substring: " + builtInSubstring);
         System.out.println("Are both substrings equal? " + areEqual);
-        scanner.close();
+        sc.close();
     }
 }
