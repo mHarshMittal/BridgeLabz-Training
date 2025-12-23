9ca8b54ccd28c2284b95b51acf6bb17761c95185
mHarshMittal <mittalharsh839@gmail.com>
2025-12-23 11:25:36 +0530

    [harsh Mittal] refactored

--- core-java-practice/gcr-codebase/array/FinfFactorsArray.java
+++ core-java-practice/gcr-codebase/array/FinfFactorsArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q10 {
+public class FinfFactorsArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
--- core-java-practice/gcr-codebase/strings/RockPaperScissor.java
+++ core-java-practice/gcr-codebase/strings/RockPaperScissor.java
@@ -3,25 +3,24 @@ import java.util.Scanner;
 public class RockPaperScissor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] opt = {"rock", "paper", "scissors"};
+        String[] opt = { "rock", "paper", "scissors" };
         int userWins = 0, compWin = 0;
 
         System.out.print("Enter number of games: ");
         int game = sc.nextInt();
 
-
         for (int i = 0; i < game; i++) {
             System.out.print("Enter your choice (rock/paper/scissors): ");
             String user = sc.next().toLowerCase();
-            String comp = opt[(int)(Math.random() * 3)];
+            String comp = opt[(int) (Math.random() * 3)];
 
             System.out.println("Computer chose: " + comp);
 
             if (user.equals(comp)) {
                 System.out.println("Draw");
             } else if ((user.equals("rock") && comp.equals("scissors")) ||
-                       (user.equals("paper") && comp.equals("rock")) ||
-                       (user.equals("scissors") && comp.equals("paper"))) {
+                    (user.equals("paper") && comp.equals("rock")) ||
+                    (user.equals("scissors") && comp.equals("paper"))) {
                 userWins++;
                 System.out.println("You win!");
             } else {
