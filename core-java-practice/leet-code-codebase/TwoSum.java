a8b31160e2c89da658b8550bbb32e2ef12625100
mHarshMittal <mittalharsh839@gmail.com>
2025-12-29 11:33:17 +0530

    [Harsh Mittal] Refactor.Refactored codes

--- core-java-practice/leet-code-codebase/TwoSum.java
+++ core-java-practice/leet-code-codebase/TwoSum.java
@@ -1,12 +1,19 @@
-public class TwoSum {
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        int[] resultArray = new int[2];
+
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+                int sum = nums[i] + nums[j];
+
+                if (sum == target) {
+                    resultArray[0] = i;
+                    resultArray[1] = j;
+                    return resultArray;
                 }
             }
         }
-        return new int[]{-1, -1}; // Return -1, -1 if targe is not found
-}
+
+        return resultArray;
+    }
 }
