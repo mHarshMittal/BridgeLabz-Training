4330d5adb3e1cbc433c103fcf9d0df45b680cb91
mHarshMittal <mittalharsh839@gmail.com>
2025-12-30 15:51:32 +0530

    [Harsh Mittal]Add.Added Clasa and Object

---
+++ oops/gcr-codebase/class-and-object/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    // main method
+    public static void main(String[] args) {
+        // print hello world to the console
+        System.out.println("Hello, World!");
+    }
+}   
