f95b9dde28bb7d381b2697db6b28c58cbd313bf0
mHarshMittal <mittalharsh839@gmail.com>
2026-01-16 12:40:46 +0530

    [harsh Mittal]Refactor. refactored code

--- dsa-practice/gcr-codebase/sorting-algorithms/HeapSort.java
+++ dsa-practice/gcr-codebase/sorting-algorithms/HeapSort.java
@@ -3,27 +3,27 @@ class HeapSort {
     public static void main(String[] args) {
         int[] a = {45000, 60000, 35000};
 
-        hs(a);
+        heapsort(a);
 
         for (int x : a) {
             System.out.print(x + " ");
         }
     }
 
-    static void hs(int[] a) {
+    static void heapsort(int[] a) {
         int n = a.length;
 
-        for (int i = n / 2 - 1; i >= 0; i--) hf(a, n, i);
+        for (int i = n / 2 - 1; i >= 0; i--) heapify(a, n, i);
 
         for (int i = n - 1; i > 0; i--) {
             int t = a[0];
             a[0] = a[i];
             a[i] = t;
-            hf(a, i, 0);
+            heapify(a, i, 0);
         }
     }
 
-    static void hf(int[] a, int n, int i) {
+    static void heapify(int[] a, int n, int i) {
         int m = i, l = 2 * i + 1, r = 2 * i + 2;
 
         if (l < n && a[l] > a[m]) m = l;
@@ -33,7 +33,7 @@ class HeapSort {
             int t = a[i];
             a[i] = a[m];
             a[m] = t;
-            hf(a, n, m);
+            heapify(a, n, m);
         }
     }
 }
