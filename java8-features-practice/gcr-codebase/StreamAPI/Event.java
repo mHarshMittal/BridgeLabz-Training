aaa3de53c0c0a04c4f33c93a4a79dbb74c448ae1
mHarshMittal <mittalharsh839@gmail.com>
2026-02-07 15:32:50 +0530

    [Harsh Mittal]Add.Added Stream API Solutions

---
+++ java8-features-practice/gcr-codebase/StreamAPI/Event.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Event {
+    public static void main(String[] args) {
+        List<String> attendees = Arrays.asList("Aman", "Priya");
+
+        attendees.forEach(a -> System.out.println("Welcome " + a));
+    }
+}
