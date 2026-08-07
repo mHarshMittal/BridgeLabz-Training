2cf9345c8d0546ed6dcc6ecb219d38721d0253bf
mHarshMittal <mittalharsh839@gmail.com>
2026-08-07 15:11:39 +0530

    [Harsh Mittal]Add.Added Strong password

---
+++ Day6/StrongPassword.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumNumber' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. STRING password
+     */
+
+    public static int minimumNumber(int n, String password) {
+    // Return the minimum number of characters to make the password strong
+    
+    boolean digit = false;
+    boolean upper = false; 
+    boolean lower = false;
+    boolean special = false;
+    
+    String specialChars = "!@#$%^&*()-+";
+    for(char ch : password.toCharArray()){
+        if(Character.isDigit(ch)){
+            digit = true;
+
+        }
+        else if(Character.isLowerCase(ch))
+            lower = true;
+        else if(Character.isUpperCase(ch))
+            upper = true;
+        else if(specialChars.indexOf(ch)!= -1)
+            special = true;
+        
+    }
+    int missing = 0;
+    if(!digit) missing++;
+    if(!lower) missing++;
+    if(!upper) missing ++;
+    if(!special) missing ++;
+    
+    return Math.max(missing, 6-n);
+    
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String password = bufferedReader.readLine();
+
+        int answer = Result.minimumNumber(n, password);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
